aeac3a10fd1010c6a8aca7564bdc8b7a3238e83d
Tazim Kolhar <tazimkolhar@gmail.com>
2017-07-31 17:40:08 +0530

    Solved Exercise 1-5
    Temperature conversion program to print the table in reverse order.

---
+++ CelsiusToFahrReverse.cpp
@@ -0,0 +1,18 @@
+// ConsoleApplication7.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "stdlib.h"
+
+// Program to print celsius and fahr. Exercise 1-5
+int main()
+{
+	for(int celsius = 300; celsius >= 0 ; celsius = celsius - 20)
+	{
+		printf("%d\t%d\n", celsius, 5 * (celsius - 32) / 9);
+		
+	}
+	system("pause");
+    return 0;
+}
+
